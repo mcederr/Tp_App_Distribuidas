9a2a0c3341dd906e6466736283b89888d7f17b42
davpad83 <davpad83@gmail.com>
2016-05-03 16:50:27 -0300

    primer commit de prueba en el proyecto base

--- rodamientos-base/src/remoto/IGestionRodamientosRemota.java
+++ rodamientos-base/src/remoto/IGestionRodamientosRemota.java
@@ -6,7 +6,7 @@ import java.rmi.RemoteException;
 import remoto.persistencia.Persistencia;
 
 public interface IGestionRodamientosRemota extends Persistencia, Remote{
-
+	//
 	public static final String SERVICIO_RMI = "//localhost/GestionRodamientos";
 	
 	public int prueba(int a) throws RemoteException;
